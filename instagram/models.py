86cca895d2c87c6c49e40d96d002ee2a76c33fca
Ruterana <ncharlene38@gmail.com>
2019-10-18 16:03:23 +0200

    iadd login

--- instagram/models.py
+++ instagram/models.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+      profile_image = models.ImageField(upload_to = 'pictures/')
+      bio= models.CharField(max_length=30)
+
+class Image(models.Model):
+    image_name= models.CharField(max_length=30)  
+    image_caption = models.TextField()
+    profile = models.ForeignKey(Profile)
+    image_path = models.ImageField(upload_to = 'pictures/')
+    
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
--- instagram/views.py
+++ instagram/views.py
@@ -2,8 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 # from django.http  import HttpResponse
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+
 def welcome(request):
     return render(request, 'welcome.html')
+
+# @login_required(login_url='/accounts/login/')
+# def image(request, image_name):
